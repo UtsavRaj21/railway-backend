a595f551f010ab3656c757afadd89f8c36a0c1e0
UtsavRaj21 <utsav11115@gmail.com>
2021-10-23 13:39:38 +0530

    update

--- routers/emptyRouter.js
+++ routers/emptyRouter.js
@@ -8,9 +8,7 @@ const getSeats = async function(req,res){
         let user = await emptyModel.findOne({"user":"Utsav"})
         res.status(200).json(user.seat)
     }catch(err){
-        res.status(500).json({
-            message:err.message
-        })
+        res.status(500).json("error")
     }
 }
 
