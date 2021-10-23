2c3f5ec42a05bd97707c44513a884296c6bd1e7c
UtsavRaj21 <utsav11115@gmail.com>
2021-10-23 15:05:24 +0530

    update

--- routers/emptyRouter.js
+++ routers/emptyRouter.js
@@ -3,6 +3,17 @@ let emptyRouter = express.Router();
 
 let emptyModel = require("../models/emptyModel")
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+    next();
+  });
+  
+
 const getSeats = async function(req,res){
     try{
         let user = await emptyModel.findOne({"user":"Utsav"})
